3aeac0904a50238a66eeeb114993abe010130775
Sylvain.D <60609924+JerkStanton@users.noreply.github.com>
2023-06-25 17:12:24 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "./style.css";
+
+//* Pour faire fonctionner les bouts de code que je te fourni, tu auras à décomenter: //__ 
+
+// Exercice 1 
+
+// Pour générer l'alphabet tu vas avoir besoin du tableau ASCII
+//__for (let i = 97; i <= 122; i++) { // ici 97 et 122 correspondent à quelque chose que tu vas trouver dans le tableau ASCII
+//__  alphabet += String.fromCharCode(i); // Ajoute la lettre correspondante à la variable alphabet
+//__}
+
+// Affichage de l'alphabet
+//__console.log(alphabet);
+
+
+
+// Exercice 2 
+
+// Récupérer l'URL complète du script en cours d'exécution
+//__const fullPath = import.meta.url;
+
+// Diviser l'URL complète en segments en utilisant le séparateur '/'
+//__const pathSegments = fullPath.split('/');
+
+// Récupérer le dernier segment qui représente le nom du fichier
+//__const fileName = pathSegments[pathSegments.length - 1];
+
+// Afficher le nom du fichier
+//__console.log(fileName);
+
+
+//Exercice 3 
+
